840fe9117859c07cf549949697eecb39c902452e
wlaurentio <gama.william@gmail.com>
2023-10-12 21:35:13 +0100

    adjusting Maps class

--- src/main/java/lessons/examples/Maps.java
+++ src/main/java/lessons/examples/Maps.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class Maps {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         Map<Integer, String> familyMembers = new HashMap<>();
 
